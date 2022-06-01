37acdc90c4b6da430a8cfae8ec188e70d7c068fe
Woody Scales <wscales10@gmail.com>
2022-06-01 14:27:20 +0100

    nuget script

--- nuget.ps1
+++ nuget.ps1
@@ -1,17 +1,156 @@
-$nuget = "D:\Program Files (x86)\NuGet\nuget.exe"
+param ($ProjectDirectory, $TopLevelDirectory = ".", [switch]$PreBuild = $false, [switch]$PostBuild = $false)
 
-foreach ($parent in Get-ChildItem . -Directory -Exclude nuget)
-{
-    foreach ($Directory in Get-ChildItem $parent -Directory)
+$nuget = "D:\Program Files (x86)\NuGet\nuget.exe"
+$xmlsettings = New-Object System.Xml.XmlWriterSettings
+$xmlsettings.Encoding = [System.Text.Encoding]::GetEncoding(1252)
+$xmlsettings.Indent = $true
+$xmlsettings.OmitXmlDeclaration = $true
+
+function Get-Xml {
+    
+    [OutputType([xml])]
+    param (
+        $File
+    )
+
+    return Get-Content $File.FullName
+}
+
+function Get-ProjectFile {
+    
+    param (
+        [string]$ProjectDirectoryString
+    )
+
+    Write-Output "Getting project file in $ProjectDirectoryString"
+
+    $ProjectFiles = Get-ChildItem "$ProjectDirectoryString\*" -Include *.csproj
+
+    
+    if ($ProjectFiles.Count -gt 1) 
     {
-        $OutputDirectory = Join-Path $directory.FullName "bin/Debug/"
-        if(Test-Path $OutputDirectory)
+        throw
+    }
+    elseif ($ProjectFiles.Count -eq 1)
+    {
+        return $ProjectFiles[0]
+    }
+    else
+    {
+        return $null
+    }
+}
+
+function Increment-Version {
+    
+    param (
+        [string]$ProjectDirectoryString
+    )
+
+    $ProjectFile = Get-ProjectFile $ProjectDirectoryString
+    
+    if ($ProjectFile -ne $null)
+    {
+        [xml]$xmlDoc = Get-Xml $ProjectFile
+            
+        $VersionNode = $xmlDoc.SelectSingleNode("/Project/PropertyGroup/Version")
+
+        if ($VersionNode -ne $null)
         {
-            foreach ($package in Get-ChildItem $OutputDirectory\* -Include *.nupkg)
+            $OldVersion = [version]$VersionNode.InnerText
+            $NewVersion = "$($OldVersion.Major).$($OldVersion.Minor).$($OldVersion.Build + 1)"
+                
+            if ($OldVersion.Revision -ne -1)
             {
-                $packageName = $package.FullName
-                & $nuget add ${packageName} -source .\nuget\packages
+                $NewVersion += ".$($OldVersion.Revision)"
+            }
+
+            $VersionNode.InnerText = $NewVersion
+            $XmlWriter = [System.XML.XmlWriter]::Create($ProjectFile.FullName, $xmlsettings)
+            $xmlDoc.Save($XmlWriter)
+        }
+    }
+}
+
+function Add-Package {
+    
+    param (
+        [string]$ProjectDirectoryString
+    )
+
+    $OutputDirectory = Join-Path $ProjectDirectoryString "bin/Debug/"
+    if(Test-Path $OutputDirectory)
+    {
+        $package = Get-ChildItem $OutputDirectory\* -Include *.nupkg | Sort-Object -Property CreationTime | Select-Object -Last 1
+
+        if ($package -ne $null)
+        {
+            $packageName = $package.FullName
+            & $nuget add ${packageName} -source $TopLevelDirectory\nuget\packages 
+        }
+    }
+}
+
+function Update-Package {
+
+    param (
+        [string]$ProjectDirectoryString
+    )
+
+    $ProjectName = (Get-ProjectFile $ProjectDirectoryString).BaseName
+
+    foreach ($SolutionDirectory in Get-ChildItem $TopLevelDirectory -Directory -Exclude nuget)
+    {
+        foreach ($OtherProjectDirectory in Get-ChildItem $SolutionDirectory -Directory)
+        {
+            $ProjectFile = Get-ProjectFile $OtherProjectDirectory.FullName
+            
+            if ($ProjectFile -ne $null)
+            {
+                [xml]$xmlDoc = Get-Xml $ProjectFile
+
+                $PackageRef = $xmlDoc.SelectSingleNode("/Project/ItemGroup/PackageReference[@Includes='$ProjectName']")
+
+                if ($PackageRef -ne $null)
+                {
+                    dotnet add $ProjectFile.FullName package $ProjectName
+                }
             }
         }
     }
+}
+
+function Execute-Steps {
+    
+    param (
+        [string]$ProjectDirectoryString
+    )
+
+    if ($PreBuild)
+    {
+        Increment-Version -ProjectDirectoryString $ProjectDirectoryString
+    }
+
+    if ($PostBuild)
+    {
+        Add-Package -ProjectDirectoryString $ProjectDirectoryString
+        Update-Package -ProjectDirectoryString $ProjectDirectoryString
+    }
+}
+
+if($ProjectDirectory -eq $null)
+{
+    Write-Output "Executing steps for all projects in $TopLevelDirectory"
+    foreach ($SolutionDirectory in Get-ChildItem $TopLevelDirectory -Directory -Exclude nuget)
+    {
+        foreach ($ProjectDirectory in Get-ChildItem $SolutionDirectory -Directory)
+        {
+            Execute-Steps -ProjectDirectoryString $ProjectDirectory.FullName
+        }
+    }
+}
+else
+{
+    Write-Output "Executing steps for $ProjectDirectory"
+    Execute-Steps -ProjectDirectoryString $ProjectDirectory
 }
