52d1c1dcdc28ade145e2efeec8b4ea34992eec51
wscales10 <wscales10@gmail.com>
2022-10-25 20:11:50 +0100

    Split in Rules project

---
+++ Common/Patterns/Patterns/Query.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Patterns.Patterns
+{
+	public static class Query<TObject>
+	{
+		public static PropertyPattern<TObject> Create<T>(string propertyName, IPattern pattern)
+		{
+			if (pattern is IPattern<T> typedPattern)
+			{
+				return Create(propertyName, typedPattern);
+			}
+
+			return Create(typeof(T), propertyName, pattern);
+		}
+
+		public static PropertyPattern<TObject> Create<T>(string propertyName, IPattern<T> pattern) => Create(typeof(T), propertyName, pattern.Simplify());
+
+		public static PropertyPattern<TObject> Create(Type type, string propertyName, IPattern pattern) => new PropertyPattern<TObject>(propertyName, type, pattern);
+	}
+}
--- MusicMod/Rules/RuleTypes/Mutable/MultiRule.cs
+++ MusicMod/Rules/RuleTypes/Mutable/MultiRule.cs
@@ -9,50 +9,50 @@ using Utils.Reflection.Properties;
 
 namespace Rules.RuleTypes.Mutable
 {
-    public class MultiRule<T, TContext, TOut> : UpperRule<TContext, TOut>, IMultiRule<T, TContext, TOut>
-    {
-        private readonly PatternGenerator<T> patternGenerator;
+	public class MultiRule<T, TContext, TOut> : UpperRule<TContext, TOut>, IMultiRule<T, TContext, TOut>
+	{
+		private readonly PatternGenerator<T> patternGenerator;
 
-        public MultiRule(string propertyName, PatternGenerator<T> patternGenerator, Func<Rule<TContext, TOut>> ruleCreator, params T[] candidates)
-        {
-            PropertyName = propertyName;
-            this.patternGenerator = patternGenerator;
-            Pairs = candidates.Select(x => (x, ruleCreator())).ToList();
-        }
+		public MultiRule(string propertyName, PatternGenerator<T> patternGenerator, Func<Rule<TContext, TOut>> ruleCreator, params T[] candidates)
+		{
+			PropertyName = propertyName;
+			this.patternGenerator = patternGenerator;
+			Pairs = candidates.Select(x => (x, ruleCreator())).ToList();
+		}
 
-        public List<(T expectedValue, Rule<TContext, TOut> rule)> Pairs { get; }
+		public List<(T expectedValue, Rule<TContext, TOut> rule)> Pairs { get; }
 
-        public string PropertyName { get; }
+		public string PropertyName { get; }
 
-        IEnumerable<(T expectedValue, IRule<TContext, TOut> rule)> IMultiRule<T, TContext, TOut>.Pairs => Pairs.Cast<(T, IRule<TContext, TOut>)>();
+		IEnumerable<(T expectedValue, IRule<TContext, TOut> rule)> IMultiRule<T, TContext, TOut>.Pairs => Pairs.Cast<(T, IRule<TContext, TOut>)>();
 
-        public static explicit operator ArrayRule<TContext, TOut>(MultiRule<T, TContext, TOut> mr)
-        {
-            return (ArrayRule<TContext, TOut>)new ArrayRule<TContext, TOut>(mr.Pairs.Select((pair) => new IfRule<TContext, TOut>(PropertyPattern<TContext>.Create(mr.PropertyName, mr.patternGenerator(pair.expectedValue)), pair.rule)).ToArray()).Named(mr.Name);
-        }
+		public static explicit operator ArrayRule<TContext, TOut>(MultiRule<T, TContext, TOut> mr)
+		{
+			return (ArrayRule<TContext, TOut>)new ArrayRule<TContext, TOut>(mr.Pairs.Select((pair) => new IfRule<TContext, TOut>(Query<TContext>.Create(mr.PropertyName, mr.patternGenerator(pair.expectedValue)), pair.rule)).ToArray()).Named(mr.Name);
+		}
 
-        public static explicit operator StaticSwitchRule<TContext, TOut>(MultiRule<T, TContext, TOut> mr)
-        {
-            return (StaticSwitchRule<TContext, TOut>)new StaticSwitchRule<TContext, TOut>(new PropertyInfo(mr.PropertyName, typeof(T)), null, mr.Pairs.Select(p => new RuleCase<TContext, TOut>(p.rule, mr.patternGenerator(p.expectedValue))).ToArray()).Named(mr.Name);
-        }
+		public static explicit operator StaticSwitchRule<TContext, TOut>(MultiRule<T, TContext, TOut> mr)
+		{
+			return (StaticSwitchRule<TContext, TOut>)new StaticSwitchRule<TContext, TOut>(new PropertyInfo(mr.PropertyName, typeof(T)), null, mr.Pairs.Select(p => new RuleCase<TContext, TOut>(p.rule, mr.patternGenerator(p.expectedValue))).ToArray()).Named(mr.Name);
+		}
 
-        public override IEnumerable<Rule<TContext, TOut>> GetRules(TContext c)
-        {
-            T seenValue = c.GetPropertyValue<T>(PropertyName);
+		public override IEnumerable<Rule<TContext, TOut>> GetRules(TContext c)
+		{
+			T seenValue = c.GetPropertyValue<T>(PropertyName);
 
-            foreach (var (expectedValue, rule) in Pairs)
-            {
-                if (patternGenerator(expectedValue).IsMatch(seenValue))
-                {
-                    yield return rule;
-                }
-            }
+			foreach (var (expectedValue, rule) in Pairs)
+			{
+				if (patternGenerator(expectedValue).IsMatch(seenValue))
+				{
+					yield return rule;
+				}
+			}
 
-            yield return null;
-        }
+			yield return null;
+		}
 
-        public override XElement ToXml() => ((StaticSwitchRule<TContext, TOut>)this).ToXml();
+		public override XElement ToXml() => ((StaticSwitchRule<TContext, TOut>)this).ToXml();
 
-        public override IReadOnlyRule<TContext, TOut> ToReadOnly(RuleParser<TContext, TOut> ruleParser) => new ReadOnlyMultiRule<T, TContext, TOut>(this, ruleParser);
-    }
+		public override IReadOnlyRule<TContext, TOut> ToReadOnly(RuleParser<TContext, TOut> ruleParser) => new ReadOnlyMultiRule<T, TContext, TOut>(this, ruleParser);
+	}
 }
