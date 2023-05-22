025d52f7e5981d8cb8d1ae790d93635ac32f1d88
wscales10 <wscales10@gmail.com>
2023-05-23 00:18:28 +0100

    Generalized build.ps1

--- MusicMod/Ror2Mod2/build.ps1
+++ MusicMod/Ror2Mod2/build.ps1
@@ -1,5 +1,15 @@
-﻿$inputDir = ".\bin\Debug\netstandard2.0"
-$outputDir = $Env:appdata + "\r2modmanPlus-local\RiskOfRain2\profiles\MyMods\BepInEx\plugins\ContextMod"
+﻿$workingDir = $Args[0]
+
+if(!$workingDir)
+{
+    $workingDir = "."
+}
+
+$profileName = $Args[1]
+$modName = $Args[2]
+
+$inputDir = Join-Path $workingDir "bin\Debug\netstandard2.0"
+$outputDir = $Env:appdata + "\r2modmanPlus-local\RiskOfRain2\profiles\"+$profileName+"\BepInEx\plugins\"+$modName
 $pdbs = Get-ChildItem -Path $inputDir\* -Include *.pdb
 $mono = "D:\Program Files\Unity\2019.4.26f1\Editor\Data\MonoBleedingEdge\bin\mono"
 
