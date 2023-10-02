3f01b31b37b368d271eeb52da833c0ba09ec5ad4
wscales10 <wscales10@gmail.com>
2023-10-02 19:01:29 +0100

    Fixed a bug which prevented Mithrix from spawning

--- MusicMod/Ror2Mod2/ContextHelper.cs
+++ MusicMod/Ror2Mod2/ContextHelper.cs
@@ -13,225 +13,226 @@ using Logger = Utils.Logger;
 
 namespace Ror2Mod2
 {
-	internal partial class ContextHelper : IContextHelper<RoR2Context>
-	{
-		private static readonly Regex bodyPrefabRegex = new Regex("BODY$");
+    internal partial class ContextHelper : IContextHelper<RoR2Context>
+    {
+        private static readonly Regex bodyPrefabRegex = new Regex("BODY$");
 
-		private readonly Logger Log;
+        private readonly Logger Log;
 
-		private int scenePart;
+        private int scenePart;
 
-		private bool isBossEncounter;
+        private bool isBossEncounter;
 
-		private RunType runType;
+        private RunType runType;
 
-		private MyScene oldScene;
+        private MyScene oldScene;
 
-		private Scene? currentScene;
+        private Scene? currentScene;
 
-		private RunOutcome? runOutcome;
+        private RunOutcome? runOutcome;
 
-		public ContextHelper(Logger logger)
-		{
-			On.RoR2.CreditsController.OnEnable += CreditsController_OnEnable;
+        public ContextHelper(Logger logger)
+        {
+            On.RoR2.CreditsController.OnEnable += CreditsController_OnEnable;
 
-			SceneManager.sceneUnloaded += (_) =>
-			{
-				oldScene = SceneName;
-				currentScene = null;
-			};
-			SceneManager.activeSceneChanged += OnSceneChanged;
+            SceneManager.sceneUnloaded += (_) =>
+            {
+                oldScene = SceneName;
+                currentScene = null;
+            };
+            SceneManager.activeSceneChanged += OnSceneChanged;
 
-			Log = logger;
+            Log = logger;
 
-			SceneCatalog.onMostRecentSceneDefChanged += SceneCatalog_onMostRecentSceneDefChanged;
+            SceneCatalog.onMostRecentSceneDefChanged += SceneCatalog_onMostRecentSceneDefChanged;
 
-			On.RoR2.TeleporterInteraction.ChargingState.OnEnter += ChargingState_OnEnter;
+            On.RoR2.TeleporterInteraction.ChargingState.OnEnter += ChargingState_OnEnter;
 
-			On.RoR2.TeleporterInteraction.IdleToChargingState.OnEnter += IdleToChargingState_OnEnter;
+            On.RoR2.TeleporterInteraction.IdleToChargingState.OnEnter += IdleToChargingState_OnEnter;
 
-			On.RoR2.TeleporterInteraction.ChargedState.OnEnter += ChargedState_OnEnter;
+            On.RoR2.TeleporterInteraction.ChargedState.OnEnter += ChargedState_OnEnter;
 
-			Application.quitting += () => NewContext?.Invoke(default);
+            Application.quitting += () => NewContext?.Invoke(default);
 
-			On.EntityStates.Missions.BrotherEncounter.PreEncounter.OnEnter += PreEncounter_OnEnter;
+            On.EntityStates.Missions.BrotherEncounter.PreEncounter.OnEnter += PreEncounter_OnEnter;
 
-			On.EntityStates.Missions.BrotherEncounter.Phase1.OnEnter += Phase1_OnEnter;
-			On.EntityStates.Missions.BrotherEncounter.BrotherEncounterPhaseBaseState.OnExit += BrotherEncounterPhaseBaseState_OnExit;
-			On.EntityStates.Missions.BrotherEncounter.EncounterFinished.OnEnter += EncounterFinished_OnEnter;
+            On.EntityStates.Missions.BrotherEncounter.Phase1.OnEnter += Phase1_OnEnter;
+            On.EntityStates.Missions.BrotherEncounter.BrotherEncounterPhaseBaseState.OnExit += BrotherEncounterPhaseBaseState_OnExit;
+            On.EntityStates.Missions.BrotherEncounter.EncounterFinished.OnEnter += EncounterFinished_OnEnter;
 
-			On.RoR2.ArtifactTrialMissionController.CombatState.OnEnter += CombatState_OnEnter;
-			On.RoR2.ArtifactTrialMissionController.WaitForRewardTaken.OnEnter += WaitForRewardTaken_OnEnter;
+            On.RoR2.ArtifactTrialMissionController.CombatState.OnEnter += CombatState_OnEnter;
+            On.RoR2.ArtifactTrialMissionController.WaitForRewardTaken.OnEnter += WaitForRewardTaken_OnEnter;
 
-			On.EntityStates.Missions.Goldshores.GoldshoresBossfight.OnEnter += GoldshoresBossfight_OnEnter;
-			On.EntityStates.Missions.Goldshores.GoldshoresBossfight.OnExit += GoldshoresBossfight_OnExit;
+            On.EntityStates.Missions.Goldshores.GoldshoresBossfight.OnEnter += GoldshoresBossfight_OnEnter;
+            On.EntityStates.Missions.Goldshores.GoldshoresBossfight.OnExit += GoldshoresBossfight_OnExit;
 
-			On.RoR2.VoidRaidEncounterController.Start += VoidRaidEncounterController_Start;
-			On.EntityStates.VoidRaidCrab.DeathState.OnExit += DeathState_OnExit;
+            On.RoR2.VoidRaidEncounterController.Start += VoidRaidEncounterController_Start;
+            On.EntityStates.VoidRaidCrab.DeathState.OnExit += DeathState_OnExit;
 
-			On.RoR2.CombatSquad.AddMember += CombatSquad_AddMember;
+            On.RoR2.CombatSquad.AddMember += CombatSquad_AddMember;
 
-			Run.onRunStartGlobal += Run_onRunStartGlobal;
-			On.RoR2.Run.BeginGameOver += Run_BeginGameOver;
-			Run.onRunDestroyGlobal += Run_onRunDestroyGlobal;
-		}
+            Run.onRunStartGlobal += Run_onRunStartGlobal;
+            On.RoR2.Run.BeginGameOver += Run_BeginGameOver;
+            Run.onRunDestroyGlobal += Run_onRunDestroyGlobal;
+        }
 
-		public event Action<RoR2Context> NewContext;
+        public event Action<RoR2Context> NewContext;
 
-		private MyScene SceneName => new MyScene(currentScene?.name?.ToUpper());
+        private MyScene SceneName => new MyScene(currentScene?.name?.ToUpper());
 
-		public void UpdateContext() => NewContext?.Invoke(GetContext());
+        public void UpdateContext() => NewContext?.Invoke(GetContext());
 
-		public RoR2Context GetContext()
-		{
-			Log("returning new Context");
-			var bossGroup = TeleporterInteraction.instance?.GetComponent<BossGroup>() ?? InstanceTracker.GetInstancesList<BossGroup>().SingleOrDefault();
-			var playerBodyPrefab = PlayerCharacterMasterController.instances.FirstOrDefault()?.master?.bodyPrefab;
+        public RoR2Context GetContext()
+        {
+            Log("returning new Context");
+            var bossGroup = TeleporterInteraction.instance?.GetComponent<BossGroup>() ?? InstanceTracker.GetInstancesList<BossGroup>().SingleOrDefault();
+            var playerBodyPrefab = PlayerCharacterMasterController.instances.FirstOrDefault()?.master?.bodyPrefab;
 
-			return new RoR2Context()
-			{
-				SceneName = SceneName,
-				SceneType = SceneCatalog.mostRecentSceneDef?.sceneType.AsEnum<MyRoR2.SceneType>() ?? MyRoR2.SceneType.Invalid,
-				StageNumber = Run.instance?.stageClearCount + 1,
-				WaveNumber = (Run.instance as InfiniteTowerRun)?.waveIndex,
-				LoopIndex = Run.instance?.loopClearCount,
-				BossBodyName = new Entity(bossGroup?.bestObservedName?.ToUpper()),
-				Bosses = (bossGroup?.combatSquad?.readOnlyMembersList?.Select(m => GetEntity(m?.bodyPrefab)) ?? Enumerable.Empty<Entity>()).ToReadOnlyCollection(),
-				TeleporterState = TeleporterInteraction.instance?.activationState.AsEnum<TeleporterState>(),
-				IsBossEncounter = isBossEncounter || !(bossGroup is null),
-				ScenePart = scenePart,
-				RunType = GetRunType(Run.instance) ?? runType,
-				Survivor = GetEntity(playerBodyPrefab),
-				Outcome = runOutcome
-			};
-		}
+            return new RoR2Context()
+            {
+                SceneName = SceneName,
+                SceneType = SceneCatalog.mostRecentSceneDef?.sceneType.AsEnum<MyRoR2.SceneType>() ?? MyRoR2.SceneType.Invalid,
+                StageNumber = Run.instance?.stageClearCount + 1,
+                WaveNumber = (Run.instance as InfiniteTowerRun)?.waveIndex,
+                LoopIndex = Run.instance?.loopClearCount,
+                BossBodyName = new Entity(bossGroup?.bestObservedName?.ToUpper()),
+                Bosses = (bossGroup?.combatSquad?.readOnlyMembersList?.Select(m => GetEntity(m?.bodyPrefab)) ?? Enumerable.Empty<Entity>()).ToReadOnlyCollection(),
+                TeleporterState = TeleporterInteraction.instance?.activationState.AsEnum<TeleporterState>(),
+                IsBossEncounter = isBossEncounter || !(bossGroup is null),
+                ScenePart = scenePart,
+                RunType = GetRunType(Run.instance) ?? runType,
+                Survivor = GetEntity(playerBodyPrefab),
+                Outcome = runOutcome
+            };
+        }
 
-		public RunType? GetRunType(Run run)
-		{
-			switch (run)
-			{
-				case null:
-					return null;
+        public RunType? GetRunType(Run run)
+        {
+            switch (run)
+            {
+                case null:
+                    return null;
 
-				case EclipseRun _:
-					return RunType.Eclipse;
+                case EclipseRun _:
+                    return RunType.Eclipse;
 
-				case InfiniteTowerRun _:
-					return RunType.Simulacrum;
+                case InfiniteTowerRun _:
+                    return RunType.Simulacrum;
 
-				case WeeklyRun _:
-					return RunType.PrismaticTrial;
+                case WeeklyRun _:
+                    return RunType.PrismaticTrial;
 
-				default:
-					return RunType.Normal;
-			}
-		}
+                default:
+                    return RunType.Normal;
+            }
+        }
 
-		private void PreEncounter_OnEnter(On.EntityStates.Missions.BrotherEncounter.PreEncounter.orig_OnEnter orig, EntityStates.Missions.BrotherEncounter.PreEncounter self)
-		{
-			scenePart = 1;
-		}
+        private void PreEncounter_OnEnter(On.EntityStates.Missions.BrotherEncounter.PreEncounter.orig_OnEnter orig, EntityStates.Missions.BrotherEncounter.PreEncounter self)
+        {
+            orig(self);
+            scenePart = 1;
+        }
 
-		private void Run_onRunDestroyGlobal(Run obj)
-		{
-			runOutcome = null;
-			UpdateContext();
-		}
+        private void Run_onRunDestroyGlobal(Run obj)
+        {
+            runOutcome = null;
+            UpdateContext();
+        }
 
-		private void Run_BeginGameOver(On.RoR2.Run.orig_BeginGameOver orig, Run self, GameEndingDef gameEndingDef)
-		{
-			orig(self, gameEndingDef);
-			runOutcome = ConvertEndingToOutcome(gameEndingDef);
-			UpdateContext();
-		}
+        private void Run_BeginGameOver(On.RoR2.Run.orig_BeginGameOver orig, Run self, GameEndingDef gameEndingDef)
+        {
+            orig(self, gameEndingDef);
+            runOutcome = ConvertEndingToOutcome(gameEndingDef);
+            UpdateContext();
+        }
 
-		private RunOutcome ConvertEndingToOutcome(GameEndingDef ending)
-		{
-			if (ending.IsOneOf(LimboEnding, ObliterationEnding, VoidEnding))
-			{
-				return RunOutcome.FateUnknown;
-			}
-			else if (ending.IsOneOf(MainEnding, PrismaticTrialEnding))
-			{
-				return RunOutcome.Victory;
-			}
-			else if (ending.IsOneOf(StandardLoss))
-			{
-				return RunOutcome.Defeat;
-			}
-			else
-			{
-				throw new NotImplementedException();
-			}
-		}
+        private RunOutcome ConvertEndingToOutcome(GameEndingDef ending)
+        {
+            if (ending.IsOneOf(LimboEnding, ObliterationEnding, VoidEnding))
+            {
+                return RunOutcome.FateUnknown;
+            }
+            else if (ending.IsOneOf(MainEnding, PrismaticTrialEnding))
+            {
+                return RunOutcome.Victory;
+            }
+            else if (ending.IsOneOf(StandardLoss))
+            {
+                return RunOutcome.Defeat;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
 
-		private void Run_onRunStartGlobal(Run obj)
-		{
-			runOutcome = RunOutcome.Undecided;
-			UpdateContext();
-		}
+        private void Run_onRunStartGlobal(Run obj)
+        {
+            runOutcome = RunOutcome.Undecided;
+            UpdateContext();
+        }
 
-		private void CombatSquad_AddMember(On.RoR2.CombatSquad.orig_AddMember orig, CombatSquad self, CharacterMaster memberMaster)
-		{
-			orig(self, memberMaster);
-			UpdateContext();
-		}
+        private void CombatSquad_AddMember(On.RoR2.CombatSquad.orig_AddMember orig, CombatSquad self, CharacterMaster memberMaster)
+        {
+            orig(self, memberMaster);
+            UpdateContext();
+        }
 
-		private void BossGroup_UpdateBossMemories(On.RoR2.BossGroup.orig_UpdateBossMemories orig, BossGroup self)
-		{
-			orig(self);
-			UpdateContext();
-		}
+        private void BossGroup_UpdateBossMemories(On.RoR2.BossGroup.orig_UpdateBossMemories orig, BossGroup self)
+        {
+            orig(self);
+            UpdateContext();
+        }
 
-		private void BossGroup_RememberBoss(On.RoR2.BossGroup.orig_RememberBoss orig, BossGroup self, CharacterMaster master)
-		{
-			orig(self, master);
-			UpdateContext();
-		}
+        private void BossGroup_RememberBoss(On.RoR2.BossGroup.orig_RememberBoss orig, BossGroup self, CharacterMaster master)
+        {
+            orig(self, master);
+            UpdateContext();
+        }
 
-		private Entity GetEntity(GameObject bodyPrefab)
-		{
-			var bodyPrefabString = bodyPrefab?.name?.ToUpper();
-			return bodyPrefabString is null ? null : new Entity(bodyPrefabRegex.Replace(bodyPrefabString, string.Empty));
-		}
+        private Entity GetEntity(GameObject bodyPrefab)
+        {
+            var bodyPrefabString = bodyPrefab?.name?.ToUpper();
+            return bodyPrefabString is null ? null : new Entity(bodyPrefabRegex.Replace(bodyPrefabString, string.Empty));
+        }
 
-		/// <summary>
-		/// Increment <see cref="scenePart"/> when
-		/// </summary>
-		/// <param name="orig"></param>
-		/// <param name="self"></param>
-		private void BrotherEncounterPhaseBaseState_OnExit(On.EntityStates.Missions.BrotherEncounter.BrotherEncounterPhaseBaseState.orig_OnExit orig, EntityStates.Missions.BrotherEncounter.BrotherEncounterPhaseBaseState self)
-		{
-			orig(self);
-			scenePart++;
-			UpdateContext();
-		}
+        /// <summary>
+        /// Increment <see cref="scenePart"/> when
+        /// </summary>
+        /// <param name="orig"></param>
+        /// <param name="self"></param>
+        private void BrotherEncounterPhaseBaseState_OnExit(On.EntityStates.Missions.BrotherEncounter.BrotherEncounterPhaseBaseState.orig_OnExit orig, EntityStates.Missions.BrotherEncounter.BrotherEncounterPhaseBaseState self)
+        {
+            orig(self);
+            scenePart++;
+            UpdateContext();
+        }
 
-		private void CreditsController_OnEnable(On.RoR2.CreditsController.orig_OnEnable orig, CreditsController self)
-		{
-			orig(self);
-			scenePart++;
-			UpdateContext();
-		}
+        private void CreditsController_OnEnable(On.RoR2.CreditsController.orig_OnEnable orig, CreditsController self)
+        {
+            orig(self);
+            scenePart++;
+            UpdateContext();
+        }
 
-		private void OnSceneChanged(Scene _, Scene newScene)
-		{
-			scenePart = 0;
-			currentScene = newScene;
-			Log($"set currentScene to {SceneName}");
+        private void OnSceneChanged(Scene _, Scene newScene)
+        {
+            scenePart = 0;
+            currentScene = newScene;
+            Log($"set currentScene to {SceneName}");
 
-			if (GetValueFromSceneName(out var newRunType, SceneName,
-				F(RunType.Simulacrum, Scenes.SimulacrumMenu),
-				F(RunType.Eclipse, Scenes.EclipseMenu),
-				F(RunType.Normal, () => ScenePattern.Equals(Scenes.MainMenu).IsMatch(oldScene), Scenes.CharacterSelect),
-				F(RunType.None, Scenes.MainMenu)
-				))
-			{
-				this.runType = newRunType;
-			}
+            if (GetValueFromSceneName(out var newRunType, SceneName,
+                F(RunType.Simulacrum, Scenes.SimulacrumMenu),
+                F(RunType.Eclipse, Scenes.EclipseMenu),
+                F(RunType.Normal, () => ScenePattern.Equals(Scenes.MainMenu).IsMatch(oldScene), Scenes.CharacterSelect),
+                F(RunType.None, Scenes.MainMenu)
+                ))
+            {
+                this.runType = newRunType;
+            }
 
-			Log($"{nameof(oldScene)}: {oldScene?.Name}");
-			Log($"{nameof(newRunType)}: {newRunType}");
-		}
-	}
+            Log($"{nameof(oldScene)}: {oldScene?.Name}");
+            Log($"{nameof(newRunType)}: {newRunType}");
+        }
+    }
 }
