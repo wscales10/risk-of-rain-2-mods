7dc0173e9b1f784a77f51c5af09e06d4f5b18026
wscales10 <wscales10@gmail.com>
2023-10-04 00:01:42 +0100

    Added build script

---
+++ MusicMod/build.ps1
@@ -0,0 +1,8 @@
+﻿param (
+    [string]$Configuration = $(throw "Configuration is required.")
+)
+
+$ErrorActionPreference = "Stop"
+
+dotnet build ..\Common\Common.sln -c $Configuration
+dotnet build MusicMod.sln -c $Configuration
