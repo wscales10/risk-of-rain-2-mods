1d9740985bb785059f8b20944c94b8edc8d13eb1
wscales10 <wscales10@gmail.com>
2023-10-10 17:38:37 +0100

    Fixed bucket bug

--- MusicMod/WPFApp/Controls/Wrappers/BucketWrapper.cs
+++ MusicMod/WPFApp/Controls/Wrappers/BucketWrapper.cs
@@ -4,29 +4,35 @@ using WPFApp.Controls.Wrappers.SaveResults;
 
 namespace WPFApp.Controls.Wrappers
 {
-	internal class BucketWrapper<TContext, TOut, TControl> : ControlWrapper<Bucket<TContext, TOut>, TControl>
-		where TControl : FrameworkElement
-	{
-		private readonly ControlWrapper<TOut, TControl> innerWrapper;
+    internal class BucketWrapper<TContext, TOut, TControl> : ControlWrapper<Bucket<TContext, TOut>, TControl>
+        where TControl : FrameworkElement
+    {
+        private readonly ControlWrapper<TOut, TControl> innerWrapper;
 
-		private Bucket<TContext, TOut> bucket;
+        private Bucket<TContext, TOut> bucket;
 
-		public BucketWrapper(ControlWrapper<TOut, TControl> innerWrapper)
-		{
-			this.innerWrapper = innerWrapper;
-		}
+        public BucketWrapper(ControlWrapper<TOut, TControl> innerWrapper)
+        {
+            this.innerWrapper = innerWrapper;
+        }
 
-		public override TControl UIElement => innerWrapper.UIElement;
+        public override TControl UIElement => innerWrapper.UIElement;
 
-		protected override void setValue(Bucket<TContext, TOut> value)
-		{
-			innerWrapper.SetValue((bucket = value).Output);
-		}
+        protected override void setValue(Bucket<TContext, TOut> value)
+        {
+            innerWrapper.SetValue((bucket = value).Output);
+        }
 
-		protected override SaveResult<Bucket<TContext, TOut>> tryGetValue(GetValueRequest request)
-		{
-			var result1 = innerWrapper.TryGetValue(request);
-			return new(result1, bucket);
-		}
-	}
+        protected override SaveResult<Bucket<TContext, TOut>> tryGetValue(GetValueRequest request)
+        {
+            var result1 = innerWrapper.TryGetValue(request);
+
+            if (result1)
+            {
+                bucket.Output = result1.Value;
+            }
+
+            return new(result1, bucket);
+        }
+    }
 }
