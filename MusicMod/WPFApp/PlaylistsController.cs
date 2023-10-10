1e57aa3687cf87a51c24a3b979159684cb9068f3
wscales10 <wscales10@gmail.com>
2023-10-10 17:37:09 +0100

    Added missing PlaylistsController file

---
+++ MusicMod/WPFApp/PlaylistsController.cs
@@ -0,0 +1,27 @@
+﻿using Spotify;
+using System.Collections.ObjectModel;
+
+namespace WPFApp
+{
+    public class PlaylistsController : NotifyPropertyChangedBase
+    {
+        private bool isEnabled;
+
+        public bool IsEnabled
+        {
+            get => isEnabled;
+
+            set
+            {
+                if (isEnabled && !value)
+                {
+                    Playlists.Clear();
+                }
+
+                SetProperty(ref isEnabled, value);
+            }
+        }
+
+        public ObservableCollection<Playlist> Playlists { get; } = new();
+    }
+}
