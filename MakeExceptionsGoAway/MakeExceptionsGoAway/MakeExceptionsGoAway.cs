1071c5c3ba8fdeebe4db29e8ab89cd211d3298c7
wscales10 <wscales10@gmail.com>
2025-12-14 15:04:08 +0000

    Naive "remove exceptions" mod

---
+++ MakeExceptionsGoAway/MakeExceptionsGoAway/MakeExceptionsGoAway.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+
+namespace MakeExceptionsGoAway
+{
+    [BepInPlugin("com.woodyscales.exceptionsbegone", "Exceptions Begone", "1.0.0")]
+    public class MakeExceptionsGoAway : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            On.RoR2.AttackSpeedPerNearbyCollider.ReconcileBuffCount += AttackSpeedPerNearbyCollider_ReconcileBuffCount;
+        }
+
+        private static void AttackSpeedPerNearbyCollider_ReconcileBuffCount(On.RoR2.AttackSpeedPerNearbyCollider.orig_ReconcileBuffCount orig, RoR2.AttackSpeedPerNearbyCollider self)
+        {
+            if (self.body is null)
+            {
+                return;
+            }
+
+            orig(self);
+        }
+    }
+}
