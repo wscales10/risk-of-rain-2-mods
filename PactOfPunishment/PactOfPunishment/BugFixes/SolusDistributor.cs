a0bd675f12f8f72bd10b56527dbae285f80f62dd
wscales10 <wscales10@gmail.com>
2026-03-22 20:58:56 +0000

    Scale MinePod mine spawn delay with attack speed
    Added SolusDistributor bug fix module to hook MinePlant state and scale mine spawn delay by attack speed stat, ensuring mines spawn faster with increased attack speed.

---
+++ PactOfPunishment/PactOfPunishment/BugFixes/SolusDistributor.cs
@@ -0,0 +1,25 @@
+﻿using EntityStates.MinePod;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+
+namespace PactOfPunishment.BugFixes
+{
+    public class SolusDistributor : Module
+    {
+        public override void Init()
+        {
+            IL.EntityStates.MinePod.MinePlant.OnEnter += Utils.HookIL(ScaleMineSpawnDelayWithAttackSpeed);
+            IL.EntityStates.MinePod.MinePlant.FixedUpdate += Utils.HookIL(ScaleMineSpawnDelayWithAttackSpeed);
+        }
+
+        private static void ScaleMineSpawnDelayWithAttackSpeed(ILCursor c)
+        {
+            while (c.TryGotoNext(MoveType.After, x => x.MatchLdsfld<MinePlant>(nameof(MinePlant.mineSpawnDelay))))
+            {
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate<Func<float, MinePlant, float>>((orig, self) => orig / self.attackSpeedStat);
+            }
+        }
+    }
+}
