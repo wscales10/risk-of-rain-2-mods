6ff26b17c95e4e9fa76f8c3df510860b52c120b1
wscales10 <wscales10@gmail.com>
2026-03-22 18:23:48 +0000

    Fix Lunar Gaze skill interruption on False Son Boss
    Move Lunar Gaze skill to use the Body state machine instead of Weapon, preventing unintended interruptions by other skills. Throws an exception if the skill is not found to ensure issues are logged.

---
+++ PactOfPunishment/PactOfPunishment/BugFixes/FalseSonBossLunarGazeInterrupt.cs
@@ -0,0 +1,25 @@
+﻿using RoR2.Skills;
+using System;
+
+namespace PactOfPunishment.BugFixes
+{
+    /// <remarks>
+    /// This module changes the False Son Boss's Lunar Gaze skill to use the Body state machine
+    /// rather than the Weapon state machine, so that it cannot be inadvertently interrupted by
+    /// other skills.
+    /// </remarks>
+    public class FalseSonBossLunarGazeInterrupt : Module
+    {
+        public override void Init()
+        {
+            SkillCatalog.skillsDefined.CallWhenAvailable(() =>
+            {
+                int lunarGazeSkillIndex = Array.IndexOf(SkillCatalog._allSkillNames, "FalseSonBossLunarGaze");
+
+                // If lunarGazeSkillIndex is -1, then I want this to throw an exception so that it
+                // is clear from the logs that the module was not applied successfully.
+                SkillCatalog._allSkillDefs[lunarGazeSkillIndex].activationStateMachineName = "Body";
+            });
+        }
+    }
+}
