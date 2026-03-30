3b5b7546013b3e27a07f2918a834e1ddcf1a0a04
wscales10 <wscales10@gmail.com>
2026-03-30 20:06:07 +0100

    Fix combat squad ownership on body split
    Add TransferCombatSquadOwnership module to set masterSummon.summonerBodyObject during BodySplitter.PerformInternal, ensuring correct ownership assignment and logging a warning if the body object is missing. This addresses issues with combat squad ownership after body splitting.

---
+++ PactOfPunishment/PactOfPunishment/BugFixes/TransferCombatSquadOwnership.cs
@@ -0,0 +1,26 @@
+﻿namespace PactOfPunishment.BugFixes
+{
+    public class TransferCombatSquadOwnership : Module
+    {
+        public override void Init()
+        {
+            On.RoR2.BodySplitter.PerformInternal += this.BodySplitter_PerformInternal;
+        }
+
+        private void BodySplitter_PerformInternal(On.RoR2.BodySplitter.orig_PerformInternal orig, RoR2.BodySplitter self, RoR2.MasterSummon masterSummon)
+        {
+            var bodyObject = self.body?.gameObject;
+
+            if (bodyObject)
+            {
+                masterSummon.summonerBodyObject = bodyObject;
+            }
+            else
+            {
+                this.Logger.LogWarning("BodySplitter has no suitable summoner body object");
+            }
+
+            orig(self, masterSummon);
+        }
+    }
+}
